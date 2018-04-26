8d7d545774f2334093e0feba639336415607ceb8
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-04-25 19:20:57 -0700

    commit all

--- moreWithMethods37/Program.cs
+++ moreWithMethods37/Program.cs
@@ -10,6 +10,22 @@ namespace moreWithMethods37
     {
         static void Main(string[] args)
         {
+            string enterNum = "Enter a number:";
+            Console.WriteLine(enterNum);
+            //int input1 = Convert.ToInt32(Console.ReadLine());
+            //double input = Convert.ToDouble(Console.ReadLine());
+            string input3 = Console.ReadLine();
+
+            //mathOperation math = new mathOperation();
+            //Console.WriteLine(math.Multiply(input1));
+
+
+            //mathOperation math2 = new mathOperation();
+            //Console.WriteLine(math2.Multiply(input2, 0));
+
+            mathOperation math3 = new mathOperation();
+            Console.WriteLine(math3.Multiply(input3, 0));
+            Console.ReadLine();
         }
     }
 }
---
+++ moreWithMethods37/mathOperation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moreWithMethods37
+{
+    class mathOperation
+    {
+        public int Multiply(int i)
+        {
+            return i * 4;
+        }
+
+        public int Multiply(double j, int i)
+        {
+            i = Convert.ToInt32(j) * 8;
+            return i;
+        }
+
+        public int Multiply(string k, int i)
+        {
+            i = int.Parse(k) * 2;
+            return i;
+        }
+    }
+}
